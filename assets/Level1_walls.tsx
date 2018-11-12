59b244d99a63ca7e506b8e1dec9c2136c5ab8bf7
joemaylin <33864215+joemaylin@users.noreply.github.com>
2018-11-12 20:29:33 +0000

    Tilesets added

---
+++ assets/Level1_walls.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.2.0" name="Level1_walls" tilewidth="32" tileheight="32" tilecount="63" columns="9">
+ <image source="WallTileset1.png" width="288" height="224"/>
+</tileset>
