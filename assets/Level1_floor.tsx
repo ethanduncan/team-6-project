e12d01d6ec15eb811c1d9ff36d556a8833b7970c
joemaylin <33864215+joemaylin@users.noreply.github.com>
2018-11-14 21:43:40 +0000

    Old maps removed, modified lvl1 to fix texture issues

--- assets/Level1_floor.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.2" tiledversion="1.2.0" name="Level1_floor" tilewidth="32" tileheight="32" tilecount="120" columns="10">
- <image source="Tileset1.png" width="320" height="384"/>
-</tileset>
--- assets/lvl1_tileset.tsx
+++ assets/lvl1_tileset.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.2" tiledversion="1.2.0" name="lvl1_tileset" tilewidth="32" tileheight="32" tilecount="350" columns="10">
- <image source="../../LabarynthTiles/lvl1_tileset.png" width="320" height="1120"/>
+ <image source="lvl1_tileset.png" width="320" height="1120"/>
 </tileset>
