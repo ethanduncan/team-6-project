b83f6333ea6e302afbafbe9d88c66a193977e333
ethanduncan <ethan.duncan-stewart@capgemini.com>
2019-01-10 14:15:39 +0000

    finally got export working

---
+++ js/menuScene.js
@@ -0,0 +1,38 @@
+var ms = new Phaser.Class({
+
+    Extends: Phaser.Scene,
+
+    initialize:
+
+    function MenuScene ()
+    {
+        Phaser.Scene.call(this, { key: 'MenuScene' });
+    },
+
+    preload: function ()
+    {
+
+    },
+
+    create: function ()
+    {
+        this.add.image(640, 320, "background");
+        this.add.image(600, 320, "logo").setScale(1.2);
+        var start = this.add.image(554, 700, "start").setScale(0.8);
+        start.setInteractive();
+        start.on("pointerdown",  () => {
+            this.menuNumber = 0;
+        });
+
+    },
+    update: function (time, delta)
+    {
+        if(this.menuNumber===0){
+            //Revert after testing
+            this.scene.start("WorldScene");
+            //this.scene.start("BossScene");
+        }
+    }
+});
+
+export { ms };
