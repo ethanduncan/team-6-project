fe0a476d2081559cffd7aeb73887a08c9da2859e
ethanduncan <ethan.duncan-stewart@capgemini.com>
2019-01-12 14:35:12 +0000

    fix healthbar

--- js/unit.js
+++ js/unit.js
@@ -1,3 +1,76 @@
+var HealthBar = new Phaser.Class({
+
+    Extends: Phaser.Class,
+
+    initialize:
+    function HealthBar (scene, x, y) {
+        this.bar = new Phaser.GameObjects.Graphics(scene);
+        this.x = x;
+        this.y = y;
+        this.value = 100;
+        this.p = 164 / 100;
+
+        this.draw();
+
+        scene.add.existing(this.bar);
+    },
+
+    decrease: function(amount) {
+        this.value -= amount;
+
+        if (this.value < 0)
+        {
+            this.value = 0;
+        }
+
+        this.draw();
+
+        return (this.value === 0);
+    },
+
+    increase: function(amount, maxHp) {
+        this.value += amount;
+
+        if (this.value > 100)
+        {
+            this.value = 100;
+        }
+
+        this.draw();
+
+        return (this.value === 0);
+
+    },
+
+    draw: function() {
+        this.bar.clear();
+
+        //  BG
+        this.bar.fillStyle(0x000000);
+        this.bar.fillRect(this.x, this.y, 170, 20);
+
+        //  Health
+
+        this.bar.fillStyle(0xffffff);
+        this.bar.fillRect(this.x + 3, this.y + 3, 164, 14);
+
+        if (this.value < 30)
+        {
+            this.bar.fillStyle(0xff0000);
+        }
+        else
+        {
+            this.bar.fillStyle(0x00ff00);
+        }
+
+        var d = Math.floor(this.p * this.value);
+
+        this.bar.fillRect(this.x + 3, this.y + 3, d, 14);
+    }
+
+});
+
+
 var unit = new Phaser.Class({
     Extends: Phaser.GameObjects.Sprite,
 
